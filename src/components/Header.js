8407ae0df4a6e520c18f114710ca2c8722e9fe69
ohnib <gv08100022@gmail.com>
2023-03-16 00:51:30 -0300

    Header component

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../components/components.css'
+
+const Header = () => {
+  return (
+    <>
+        <header>
+          <div className='header-container'>
+            <h1 className='header-logo'>GABRIEL</h1>
+            <nav className='header-nav'>
+              <ul>
+                <li><a href='#'>ABOUT ME</a></li>
+                <li><a href='#'>EXPERIENCE & SKILLS</a></li>
+                <li><a href='#'>CONTACTS</a></li>
+              </ul>
+            </nav>
+          </div> 
+
+        </header>
+    </>
+  )
+}
+
+export default Header
