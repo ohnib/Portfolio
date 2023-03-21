388edba252991ae2c53a08d6325a21657f4355a6
ohnib <gv08100022@gmail.com>
2023-03-21 11:11:44 -0300

    Mobile Menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,24 @@ import React from 'react'
 import '../components/css/header.css'
 
 const Header = () => {
+  const [open, setOpen] = React.useState(false);
+  function toggleMenu()
+  {
+    setOpen(!open)
+  }
+
   return (
     <>
         <header>
           <div className='header-container'>
             <h1 className='header-logo'>GABRIEL</h1>
-            <nav className='header-nav'>
-              <ul>
+            <nav className='header-nav menu-ativo'>
+              <div className='menu-mobile-container'  onClick={toggleMenu}>
+                <span className={open ? 'menu-mobile' : 'menu-mobile-close'}>
+
+                </span>
+              </div>
+              <ul className={open ? 'header-none' : ''}>
                 <li><a href='/Aboutme'>ABOUT ME</a></li>
                 <li><a href='/Experience'>EXPERIENCE & SKILLS</a></li>
                 <li><a href='/Contacts'>CONTACTS</a></li>
