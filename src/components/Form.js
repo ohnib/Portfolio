ae303e8a3d5166ce61b648e2f766341bf04aa7c4
ohnib <gv08100022@gmail.com>
2023-03-23 17:17:37 -0300

    Form submit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import '../components/css/form.css'
 
 const Form = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_1g9rbcl', 'template_jbhzvd8', form.current, 'ccnKAF7OyTlHGlaz2')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <>
-        <div className='form-container'>
-            <form>
-                <label htmlFor='name'>NAME</label>
-                <input id='name' type='text'/>
+      <div className='form-container'>
+        <form ref={form} onSubmit={sendEmail}>
+          <label htmlFor='name'>Name</label>
+          <input type="text" name="user_name" id='name'/>
 
-                <label htmlFor='email'>EMAIL</label>
-                <input id='email' type='email'/>
-            </form>
-            <div className='form-message'>
-            <h2 style={{margin: '60px 0 0 0', color:'var(--solida1)'}}>MESSAGE ME</h2>
-            <textarea/>
-            <button>ENVIAR</button>
-            </div>
-        </div>
+          <label htmlFor='email'>Email</label>
+          <input type="email" name="user_email" id='email'/>
+          
+          <label className='form-message'>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="submit" />
+        </form>
+      </div>
     </>
   )
 }
